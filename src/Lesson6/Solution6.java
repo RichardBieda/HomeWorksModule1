89ec43d2377de494cf1a1a96fefccdc3e9045de4
Richard <seter74@gmail.com>
2024-10-25 23:56:50 +0200

    First commit

--- src/Lesson6/Solution6.java
+++ src/Lesson6/Solution6.java
@@ -5,29 +5,15 @@ import java.util.Scanner;
 
 public class Solution6 {
 
-    public static void main(String[] args) {
-
-        // Write a program to find the index of an array element. (-1 if not found)
-          int[] array = {5, 88, -7, 9, 0, 12};
-          int[][] multidimensionalArray = {{3, 6, 9}, {4, 8, 12}};
-
-          Scanner scanner = new Scanner(System.in);
-          int number = scanner.nextInt();
-          System.out.println(getIndex(array, number));
-          int number2 = scanner.nextInt();
-          System.out.println(Arrays.toString(getDeepIndex(multidimensionalArray, number2))); // Arrays.toString is a static method of the Arrays class to print array indeces.
-
-    }
-
     public static int getIndex(int[] arr, int value) { //Method for searching the index
         int index = -1;
 
-           for (int i = 0; i < arr.length; i++) {
-                  if (value == arr[i]) {
-                     index = i;
-                      break;
-                  }
-           }
+        for (int i = 0; i < arr.length; i++) {
+            if (value == arr[i]) {
+                index = i;
+                break;
+            }
+        }
         return index;
     }
 
@@ -44,5 +30,22 @@ public class Solution6 {
             }
         }
         return index;
+    }
+    public static void main(String[] args) {
+
+        // Write a program to find the index of an array element. (-1 if not found)
+          int[] array = {5, 88, -7, 9, 0, 12};
+          int[][] multidimensionalArray = {{3, 6, 9}, {4, 8, 12}};
+
+          Scanner scanner = new Scanner(System.in);
+          int number = scanner.nextInt();
+          System.out.println(getIndex(array, number));
+          int number2 = scanner.nextInt();
+          System.out.println(Arrays.toString(getDeepIndex(multidimensionalArray, number2))); // Arrays.toString is a static method of the Arrays class to print array indeces.
+
+        //------------------------------------------------------------------------------------------------
+        // Write a program to find the number of even and odd integers in a given array of integers.
+
+
     }
 }
