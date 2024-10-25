a84a16bb32740aca9fb8ff3511fca7570e92c86d
RichardBieda <seter74@gmail.com>
2024-10-26 00:49:56 +0200

    Update LazyEvaluationExemple.java

--- src/Lesson6/LazyEvaluationExemple.java
+++ src/Lesson6/LazyEvaluationExemple.java
@@ -9,13 +9,13 @@ public class LazyEvaluationExemple {
         int bigNumber = 100000;
 
         if (0 < 500 && (number += 10) < bigNumber) {
-            System.out.printf("(number + 10) was executed number is now %d\n", number);
+            System.out.printf("(number + 10) was executed, number is now %d\n", number);
         }
 
         if (500 < 0 && (number += 10) < bigNumber) {
 
         } else {
-            System.out.printf("(number + 10) wasn't executed number is still %d\n", number);
+            System.out.printf("(number + 10) wasn't executed, number is still %d\n", number);
         }
     }
 }
